791b556a636fb0f468ad9985b4e24cf9581444ed
anuragshukla06 <33591225+anuragshukla06@users.noreply.github.com>
2018-11-18 00:40:29 +0530

    Add files via upload

---
+++ java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,41 @@
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("2dd65b9333c34e71225e4feb5882e1a756ad894e")
+            .clientKey("d761a43c3a05d004da901297fd86edf59e81cace")
+            .server("http://13.233.162.83:80/parse/")
+            .build()
+    );
+
+
+
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
